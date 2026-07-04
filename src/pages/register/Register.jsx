e5d2ef157923939b8d4eddec106d47205f08c8d7
ali-albaz <alibaz.ronin@gmail.com>
2026-07-04 16:45:06 +0300

    server-side-validation-DisplyErrors-register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
@@ -7,10 +7,13 @@ import { useForm } from 'react-hook-form'
 import axios from 'axios'
 import { yupResolver } from '@hookform/resolvers/yup';
 import { registerSchema } from '../../validations/RegisterSchema'
+import { CircularProgress } from '@mui/material'
 
 export default function Register() {
 
-  const {register,handleSubmit,formState:{errors}} = useForm(
+  const[serverErrors,setServerErrors] = useState([]);
+
+  const {register,handleSubmit,formState:{errors,isSubmitting}} = useForm(
     {
       resolver:yupResolver(registerSchema)
     }
@@ -22,13 +25,17 @@ export default function Register() {
       const response = await axios.post(`${import.meta.env.VITE_BURL}/auth/Account/Register`,data);
       console.log(response.data);
     } catch (err) {
-      console.log(err);
+      setServerErrors(err.response.data.errors);
     }
   }
 
   return (
     <Box component="section">
       <Typography component="h1" variant="h2">Register</Typography>
+
+      {serverErrors?.length > 0 ? serverErrors.map( (error)=>
+       <Typography color="error">{error}</Typography>
+      ) :''}
 
       <Box component="form" onSubmit={handleSubmit(RegisterForm)}>
 
@@ -52,8 +59,7 @@ export default function Register() {
         error={errors.phoneNumber}
         helperText={errors.phoneNumber?.message}/> 
 
-        <Button variant="outlined" type="submit">Register</Button>
-
+        <Button variant="outlined" type="submit" disabled={isSubmitting}>{isSubmitting? <CircularProgress /> :'Register'}</Button>
       </Box>
     </Box>
   )

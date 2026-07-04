dec4274abfff862a6c60eae4f2835b0105007045
ali-albaz <alibaz.ronin@gmail.com>
2026-07-04 16:19:08 +0300

    client-side-validation-register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -5,10 +5,17 @@ import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
+import { yupResolver } from '@hookform/resolvers/yup';
+import { registerSchema } from '../../validations/RegisterSchema'
 
 export default function Register() {
 
-  const {register,handleSubmit} = useForm()
+  const {register,handleSubmit,formState:{errors}} = useForm(
+    {
+      resolver:yupResolver(registerSchema)
+    }
+  )
+
 
   const RegisterForm = async(data)=>{
     try {
@@ -24,12 +31,29 @@ export default function Register() {
       <Typography component="h1" variant="h2">Register</Typography>
 
       <Box component="form" onSubmit={handleSubmit(RegisterForm)}>
-        <TextField {...register("userName")} sx={{margin:1}} label="userName" variant="outlined" />
-        <TextField {...register("fullname")} sx={{margin:1}} label="fullname" variant="outlined" /> 
-        <TextField {...register("email")} sx={{margin:1}} label="email" variant="outlined" />
-        <TextField {...register("password")} sx={{margin:1}} label="password" variant="outlined" />
-        <TextField {...register("phoneNumber")} sx={{margin:1}} label="phoneNumber" variant="outlined" /> 
+
+        <TextField {...register("userName")} sx={{margin:1}} label="userName" variant="outlined" 
+        error={errors.userName}
+        helperText={errors.userName?.message}/>
+
+        <TextField {...register("fullname")} sx={{margin:1}} label="fullname" variant="outlined" 
+        error={errors.fullname}
+        helperText={errors.fullname?.message}/> 
+
+        <TextField {...register("email")} sx={{margin:1}} label="email" variant="outlined" 
+        error={errors.email}
+        helperText={errors.email?.message}/>
+
+        <TextField {...register("password")} sx={{margin:1}} label="password" variant="outlined" 
+        error={errors.password}
+        helperText={errors.password?.message}/>
+
+        <TextField {...register("phoneNumber")} sx={{margin:1}} label="phoneNumber" variant="outlined" 
+        error={errors.phoneNumber}
+        helperText={errors.phoneNumber?.message}/> 
+
         <Button variant="outlined" type="submit">Register</Button>
+
       </Box>
     </Box>
   )
